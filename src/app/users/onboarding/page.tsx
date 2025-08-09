fda68e763594a53bc15dbef803d615f08b271dad
cliff76 <270462+cliff76@users.noreply.github.com>
2025-08-09 14:58:46 -0700

    work in progress

--- src/app/users/onboarding/page.tsx
+++ src/app/users/onboarding/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { useState } from "react";
+import {useState} from "react";
 import {useSignUp, useUser} from "@clerk/nextjs";
-import { useRouter } from "next/navigation";
+import {useRouter} from "next/navigation";
 import {RegisteredUser} from "@/app/users/utils";
-import {createUser} from "@/app/users/actions/create";
+import {createUser, updateUser} from "@/app/users/actions/create";
 import {UserResource} from "@clerk/types";
 
 type InitialFormProps = {
@@ -211,7 +211,7 @@ function InitialForm({isLoading, user, error, onNext} : InitialFormProps) {
     </div>;
 }
 
-function SequentialForm({isLoading, user, error, onNext} : InitialFormProps) {
+function SequentialForm({isLoading, error, onNext} : InitialFormProps) {
     const [danger, setDanger] = useState<"yes" | "no" | "">("");
     const [admitAlcoholic, setAdmitAlcoholic] = useState(false);
     const [committedToRecovery, setCommittedToRecovery] = useState(false);
@@ -404,65 +404,105 @@ function SequentialForm({isLoading, user, error, onNext} : InitialFormProps) {
     </div>;
 }
 
+async function handleStep1(formData: FormData, onError: (error: string) => void) {
+    const initialFormRequiredFields = ['firstName', 'lastName', 'phone', 'sobrietyDate', 'sponsor'];
+    try {
+        //check required fields
+        initialFormRequiredFields.forEach((requiredField) => {
+            if (!formData.get(requiredField)) {
+                onError(`${requiredField} is required`);
+                return;
+            }
+        });
+
+        //create the user object
+        const userData: RegisteredUser = {
+            firstName: formData.get('firstName')?.toString() ?? '',
+            lastName: formData.get('lastName')?.toString() ?? '',
+            primaryEmailAddress: formData.get('email')?.toString() ?? '',
+            phone: formData.get('phone')?.toString() ?? '',
+            sobrietyDate: formData.get('sobrietyDate')?.toString() ?? '',
+            sponsor: formData.get('sponsor')?.toString() ?? '',
+            currentStep: formData.get('step')?.toString() ?? ''
+        };
+        const response = await createUser(userData);
+
+        if (response.ok) {
+            return true;
+        } else {
+            onError("Failed to save users metadata: " + response.errors?.join(',\n'));
+            return false;
+        }
+
+    } catch (err: unknown) {
+        onError((err as Error)?.message || "An error occurred during sign up");
+        return false;
+    }
+}
+
+async function handleStep2(formData: FormData, onError: (error: string) => void, user?: UserResource | null): Promise<boolean> {
+    try {
+        if (!user) {
+            onError("User is not logged in.");
+            return false;
+        }
+        const result = await updateUser(user.id, formData);
+        // Redirect to dashboard after successful signup
+        if (result.ok) {
+            return true;
+        } else {
+            onError("Failed to update user. " + result.errors?.join(',\n'));
+            return false;
+        }
+    } catch (e: unknown) {
+        onError("Failed to update user [" + ((e as Error).message ? (e as Error).message : e) + "]");
+        return false;
+    } finally {
+        await user?.reload();
+    }
+}
+
 export default function OnboardingPage() {
     const { isLoaded } = useSignUp();
     const router = useRouter();
 
-    // Form states
-
     const [error, setError] = useState("");
     const [isLoading, setIsLoading] = useState(false);
     const { user } = useUser();
-    const initialFormRequiredFields = ['firstName', 'lastName', 'phone', 'sobrietyDate', 'sponsor'];
     const [step, setStep] = useState<number>(1);
 
-    const handleInitialFormSubmit = async (formData: FormData) => {
+    function onError(error: string) {
+        console.error(error);
+        setError(error);
+    }
+
+    const handleSubmit = async (formData: FormData) => {
 
         if (!isLoaded) return;
 
         try {
-            setIsLoading(true);
             setError("");
-            //check required fields
-            initialFormRequiredFields.forEach((requiredField) => {
-                if(!formData.get(requiredField)) {
-                    setError( `${requiredField} is required`);
-                    return;
+            setIsLoading(true);
+            if (step === 1) {
+                const result = await handleStep1(formData, onError);
+                if(result) {
+                    setStep(2);
                 }
-            });
-
-            //create the user object
-            const userData: RegisteredUser = {
-                firstName: formData.get('firstName')?.toString() ?? '',
-                lastName: formData.get('lastName')?.toString() ?? '',
-                primaryEmailAddress: formData.get('email')?.toString() ?? '',
-                phone: formData.get('phone')?.toString() ?? '',
-                sobrietyDate: formData.get('sobrietyDate')?.toString() ?? '',
-                sponsor: formData.get('sponsor')?.toString() ?? '',
-                currentStep: formData.get('step')?.toString() ?? ''
-            };
-            const response = await createUser(userData);
-
-            if (response.ok) {
-                await user?.reload();
-                // Redirect to dashboard after successful signup
-                // router.push("/");
-                setStep(2);
-            } else {
-                setError("Failed to save users metadata: " + response.errors?.join(',\n'));
             }
-
-
-        } catch (err: unknown) {
-            setError((err as Error)?.message || "An error occurred during sign up");
+            if (step === 2) {
+                const result = await handleStep2(formData, onError, user);
+                if(result) {
+                    router.push('/');
+                }
+            }
         } finally {
             setIsLoading(false);
         }
     };
 
     if(step === 1)  return (
-        <InitialForm user={user} isLoading={isLoading || !isLoaded} error={error} onNext={handleInitialFormSubmit}/>
+        <InitialForm user={user} isLoading={isLoading || !isLoaded} error={error} onNext={handleSubmit}/>
     );
     if(step === 2) return (
-        <SequentialForm user={user} isLoading={isLoading || !isLoaded} error={error} onNext={handleInitialFormSubmit}/>
+        <SequentialForm user={user} isLoading={isLoading || !isLoaded} error={error} onNext={handleSubmit}/>
     )}
