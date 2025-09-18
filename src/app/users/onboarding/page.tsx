fc5f8f817a571831532271f18165b79e92a710b5
Clifton <270462+cliff76@users.noreply.github.com>
2025-09-18 13:44:24 -0700

    Removes logging. (#10)

--- src/app/users/onboarding/page.tsx
+++ src/app/users/onboarding/page.tsx
@@ -36,7 +36,6 @@ export default function OnboardingPage() {
                 return;
             }
             if (step === 1) {
-                console.log('onboarding/page.tsx: handleStep1');
                 const result = await handleStep1(user.id ?? '', formData, user?.publicMetadata?.roles ?? [], onError);
                 setHasHope(formData.get('resident') === 'existing');
                 if(result) {
@@ -53,7 +52,6 @@ export default function OnboardingPage() {
             setIsLoading(false);
         }
     };
-    console.log('onboarding/page.tsx: rendering onboarding page...');
 
     if(step === 1)  return (
         <InitialForm user={user} isLoading={isLoading || !isLoaded} error={error} onNext={handleSubmit}/>
